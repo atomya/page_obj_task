c9cb37fdc39a0cb276743fcdfc5338087c64e9ee
atomya <yana12340@gmail.com>
2020-04-28 18:43:53 +0300

    Initial commit

---
+++ conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        print("Browser {} still is not implemented".format(browser_name))
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
